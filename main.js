d2cc1406b02bd0c19f1669d24551d238f0ca96b6
Neil Justin <mallarineil9@gmail.com>
2022-08-04 11:33:26 +0800

    Simplify buttons alignment and add click listener to numbers btn

--- main.js
+++ main.js
@@ -13,3 +13,11 @@ function multiply(x, y) {
 function divide(x, y) {
     return x / y;
 }
+
+const currentValueDiv = document.querySelector('.current-value');
+const numbersBtn = document.querySelectorAll('.numbers-btn');
+numbersBtn.forEach(number => {
+    number.addEventListener('click', () => {
+        currentValueDiv.textContent = `${number.value}`
+    });
+});

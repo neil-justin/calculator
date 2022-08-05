4e552911d9a666911a2486b0c71630bc5ac7269f
Neil Justin <mallarineil9@gmail.com>
2022-08-05 14:37:24 +0800

    Add a click listener to operators-btn and display them when clicked

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
+let currentVal = 0;
 const currentValDiv = document.querySelector('.current-value');
-const currentVal = parseInt(currentValDiv);
 const numbersBtn = document.querySelectorAll('.numbers-btn');
 numbersBtn.forEach(numberBtn => {
     numberBtn.addEventListener('click', () => {
         if (currentValDiv.innerText === '0') {
             if (numberBtn.value === '0') {
-                currentValDiv.textContent = 0;
+                currentVal = 0;
+                currentValDiv.textContent = currentVal;
             } else {
-                currentValDiv.textContent = `${numberBtn.value}`;
+                currentVal = `${numberBtn.value}`;
+                currentValDiv.textContent = currentVal;
             }
         } else {
             if (currentValDiv.innerText.length > 16) {
@@ -15,11 +17,27 @@ numbersBtn.forEach(numberBtn => {
                     numberBtn.disabled = true;
                 });
             } else {
-                currentValDiv.textContent += `${numberBtn.value}`;
+                currentVal += `${numberBtn.value}`;
+                currentValDiv.textContent = currentVal;
             }
         }
     });
 });
+
+const storedValDiv = document.querySelector('.stored-value');
+
+let operatorVal;
+const operatorsBtn = document.querySelectorAll('.operators-btn');
+operatorsBtn.forEach(operatorBtn => {
+    operatorBtn.addEventListener('click', () => {
+        operatorVal = operatorBtn.value;
+        storedValDiv.textContent = `${currentVal} ${operatorBtn.innerText}`;
+    })
+})
+
+let currentVal2;
+
+
 
 function add(x, y) {
     return x + y;
